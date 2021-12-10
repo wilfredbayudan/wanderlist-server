591b795e77adf54de213e6675019d71725eab3ea
Wilfred <wilfredbayudan@gmail.com>
2021-12-09 15:34:48 -1000

    Added 'like' and 'dislike' instance methods to destinations

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -2,4 +2,17 @@ class Destination < ActiveRecord::Base
   has_many :comments
   has_many :bucketlist_destinations
   has_many :bucketlists, through: :bucketlist_destinations
+
+  def like
+    self.likes += 1;
+    self.save
+    self.likes
+  end
+
+  def dislike
+    self.likes -= 1;
+    self.save
+    self.likes
+  end
+
 end
